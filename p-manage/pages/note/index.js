02ed938510247c387b4a3b4ec5a8f67d07c5c79a
yijie.mo <yijie.mo@vipshop.com>
2020-07-09 15:54:12 +0800

    日记编辑页面

--- p-manage/pages/note/index.js
+++ p-manage/pages/note/index.js
@@ -1,6 +1,8 @@
 const computedBehavior = require('miniprogram-computed');
 const apiBehavior = require('../../../behaviors/api.js');
 
+import Toast from '@vant/weapp/toast/toast';
+
 Page({
   behaviors: [computedBehavior, apiBehavior],
 
@@ -10,14 +12,44 @@ Page({
   data: {
     id: 0,
     eventBrief: "",
-    noetDetail: "",
-    noetConclusion: "",
+    noteDetail: "",
+    noteConclusion: "",
     shareList: []
   },
 
+  onLoad: function (options) {
+    if (options.id === undefined) {
+      return ;
+    }
+    this.requestData(options.id);
+  },
+
+  requestData: function(id) {
+    this.post('getNote', id, this.afterFetch.bind(this));
+  },
+
+  afterFetch: function(data) {
+    var shareList = [];
+    if (data.relatedShareList !== undefined) {
+      for (var i in data.relatedShareList) {
+        shareList.push({
+          tsCode: data.relatedShareList[i].tsCode,
+          shareName: data.relatedShareList[i].shareName
+        })
+      }
+    }
+    this.setData({
+      id: data.id,
+      eventBrief: data.eventBrief,
+      noteDetail: data.noteDetail,
+      noteConclusion: data.noteConclusion,
+      shareList: shareList
+    });
+  },
+
   onShareChosen: function(e) {
-    var tsCode = event.detail.tsCode;
-    var shareName = event.detail.shareName;
+    var tsCode = e.detail.tsCode;
+    var shareName = e.detail.shareName;
 
     var exists = false;
     for (var i in this.data.shareList) {
@@ -30,9 +62,64 @@ Page({
 
     if (!exists) {
       var arr = this.data.shareList;
-      arr = [];
-      arr.append({tsCode, shareName});
+      arr.push({tsCode, shareName});
       this.setData({shareList: arr});
     }
+  },
+
+  removeShare: function(e) {
+    var toRemove = e.currentTarget.dataset.code;
+    var newarr = [];
+    for (var i in this.data.shareList) {
+      var item = this.data.shareList[i];
+      if (item.tsCode === toRemove) {
+        continue;
+      }
+      newarr.push(item);
+    }
+    this.setData({shareList: newarr});
+  },
+
+  saveNote: function(e) {
+    var param = {
+      eventBrief: this.data.eventBrief,
+      noteDetail: this.data.noteDetail,
+      noteConclusion: this.data.noteConclusion
+    };
+    if (this.data.id != 0) {
+      param.id = this.data.id;
+    }
+    var codeList = [];
+    for (var i in this.data.shareList) {
+      var item = this.data.shareList[i];
+      codeList.push(item.tsCode);
+    }
+    param.tsCodeList = codeList;
+    var url = this.data.id != 0 ? 'manage/editNote' : 'manage/addNote';
+    this.post(url, param, this.afterSave.bind(this));
+  },
+
+  afterSave: function(data) {
+    console.log(data);
+    var opts = {
+      selector: '#manage-note-toast',
+      message: data.msg
+    };
+    if (data.success === true) {
+      Toast.success(opts);
+      if (this.data.id === 0) {
+        this.setData({
+          id: data.data
+        });
+      }
+    } else {
+      Toast.fail(opts);
+    }
+  },
+
+  updateInput: function(e) {
+    var data = {};
+    data[e.currentTarget.dataset.name] = e.detail;
+    this.setData(data);
   }
 })
